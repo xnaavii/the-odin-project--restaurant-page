13524658574f272c6409e6c72f05ef1c5860a2fd
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-09-05 14:06:35 +0100

    Add change active page func

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import initialPageLoad from "./initialPageLoad.js";
 import menuPage from "./menuPage.js";
 import aboutPage from "./aboutPage.js";
 
-const pages = { home: initialPageLoad, menu: menuPage, about: aboutPage };
 const navBtn = document.querySelectorAll(".nav-btn");
 
 navBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    const pageName = e.target.dataset.page;
-    if (!pageName || !pages[pageName]) return;
     clearContent();
-    pages[pageName]();
+    changeActivePage(e.target.dataset.page);
   });
 });
+
+function changeActivePage(pageName) {
+  const pages = { home: initialPageLoad, menu: menuPage, about: aboutPage };
+  if (!pageName || !pages[pageName]) return;
+  pages[pageName]();
+}
 
 function clearContent() {
   const content = document.querySelector("#content");
