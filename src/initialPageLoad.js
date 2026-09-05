12aef4eacf5919757a5a0b7ddc628bbb7a27ad4c
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-09-05 09:12:15 +0100

    Style the hero section

--- src/initialPageLoad.js
+++ src/initialPageLoad.js
@@ -3,11 +3,17 @@ import heroImg from "./assets/hero-image.jpg";
 export default function initialPageLoad() {
   const contentDiv = document.querySelector("#content");
 
+  const heroContainer = document.createElement("div");
+  heroContainer.classList.add("hero-container");
+
   const title = document.createElement("h1");
+  title.classList.add("hero-title");
   title.textContent = "Welcome to the best restaurant in Dublin";
-  contentDiv.append(title);
+  heroContainer.append(title);
 
   const img = new Image();
   img.src = heroImg;
-  contentDiv.append(img);
+  heroContainer.append(img);
+
+  contentDiv.append(heroContainer);
 }
