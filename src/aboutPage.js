18fac4942b093120ddf15b14ce70db553f1b02e1
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-09-06 17:44:14 +0100

    Update the about page

--- src/aboutPage.js
+++ src/aboutPage.js
@@ -2,6 +2,12 @@ export default function aboutPage() {
   const contentDiv = document.querySelector("#content");
 
   const title = document.createElement("h1");
-  title.textContent = "Learn more about the best pizza place";
+  title.textContent = "Learn more about THE pizza place!";
+
+  const details = document.createElement("p");
+  details.textContent =
+    "In order to know more you gotta try it, so order one now! 🍕";
+
   contentDiv.append(title);
+  contentDiv.append(details);
 }
